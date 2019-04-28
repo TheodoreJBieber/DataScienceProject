49edb6ccd16b18a8659c5137e937a96e1a2c0fba
jbbieber1127 <jbieber1127@gmail.com>
2019-04-27 23:33:28 -0400

    updated trimdata to create correct fips codes, now colors the county map and on hover prints to console. todo: tooltips on hover

--- js/counties_choropleth.js
+++ js/counties_choropleth.js
@@ -13,18 +13,41 @@ d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
 
   // path.projection(projection);
   // projection.fitExtent([[0,0],svgextent], topojson.feature(us, us.objects.counties).features);
-  d3.json("", function(error, counties){
-
+  d3.json("dataset/county_counts.json", function(error, counties){
+    let min = -1;
+    let max = -1;
+    console.log(counties)
+    for(let county in counties){
+      county = counties[county];
+      console.log(county);
+      if(county.count == undefined){
+        continue;
+      }
+      if(min == -1 || county.count < min){
+        min = county.count;
+      }
+      if(max == -1 || county.count > max){
+        max = county.count;
+      }
+    }
+    console.log('min' + min)
+    let colorscheme = d3.schemeReds[9];
+    let col = d3.scaleQuantize()
+        .domain([min, max])
+        .range(colorscheme)
+    svg.append("g")
+        .attr("class", "counties")
+      .selectAll("path")
+      .data(topojson.feature(us, us.objects.counties).features)
+      .enter().append("path")
+        .attr("d", path)
+        .style("fill", function(d){return counties[d.id] ? col(counties[d.id].count) : col(min)})
+        .on('mouseover', function(d){
+          console.log(counties[d.id] ? counties[d.id].count : 0);
+        });
   });
-  svg.append("g")
-      .attr("class", "counties")
-    .selectAll("path")
-    .data(topojson.feature(us, us.objects.counties).features)
-    .enter().append("path")
-      .attr("d", path)
-      .style("fill", function(d){console.log(d)});
 
-  svg.append("path")
-      .attr("class", "county-borders")
-      .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; })));
+  // svg.append("path")
+  //     .attr("class", "county-borders")
+  //     .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; })));
 });
